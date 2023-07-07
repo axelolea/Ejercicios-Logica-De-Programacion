8f802a4b2c0bdeb40a57db5605cb1edd7f7a0313
axelolea <axelcedilloolea@gmail.com>
2023-07-06 20:54:51 -0600

    Agregando modificaciones de primer ejercicio y gitignore

--- Ejercicio de Lógica de Programación 1/main.js
+++ Ejercicio de Lógica de Programación 1/main.js
@@ -12,12 +12,15 @@ const [
     "#reverse-output"
 ].map(selector => document.querySelector(selector))
 
-// Verificar si un dato es un numero
+// Resetar el array y pedirlo de nuevo
+const resetBtn = document.querySelector("#reset")
+
+// Funcion para verificar si un dato es un numero
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-
+// Creacion de una carta con un valor y retornar el elemento
 function crearCarta(num){
     const card = document.createElement("div")
     
@@ -27,28 +30,30 @@ function crearCarta(num){
     return card
 }
 
-
+// Funcion para obtener valores numericos y devolver un array 
 function getNumberArray(){
+    // El array 
     let miNumbers = [];
-
+    // Salir del bucle hasta que el array tenga la longitud desseada 
     while(miNumbers.length < numeroElementos){
-
+        // Prompt para obtener el numero 
         const value = prompt(`Ingrese un numero (faltan ${numeroElementos - miNumbers.length}):`)
 
-        if(isNumeric(value)){
-            miNumbers.push(parseInt(value))
-        }
-        else {
+        // Verificar si es no es un numero
+        if(!isNumeric(value)){
             alert(`El numero ${value}, no es un numero valido`)
+            continue;
         }
-
+        // Ingresar el numero al array 
+        miNumbers.push(parseInt(value))
     }
-
+    // Al salir del bucle devolver el array 
     return miNumbers
 }
 
 function run(){
     
+    // Obtener el array generados por prompt, ordenarlos y invertirlos 
     const arrayNumeros = getNumberArray()
     const sortNumeros = arrayNumeros.toSorted()
     const reverseNumeros = arrayNumeros.toSorted().toReversed()
@@ -58,11 +63,20 @@ function run(){
     const cardItemsSorted = sortNumeros.map(num => crearCarta(num))
     const cardItemsReversed = reverseNumeros.map(num => crearCarta(num))
 
+    // Limpiando los container 
+    containerInput.innerHTML = "";
+    containerOutput.innerHTML = "";
+    containerReverse.innerHTML = "";
+
+    // Ingresar los elemmentos del array de elementos 
     cardItems.forEach(el => containerInput.append(el))
     cardItemsSorted.forEach(el => containerOutput.append(el))
     cardItemsReversed.forEach(el => containerReverse.append(el))
 
 }
 
-
+// Correr la funcion ejecutora
 run()
+
+// Volver a ejecutar el codigo con el boton reset 
+resetBtn.addEventListener('click', run)
